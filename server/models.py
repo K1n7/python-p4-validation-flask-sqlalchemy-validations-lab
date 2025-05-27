b541627061640a80eeaeb33f0784875e1cd03b73
Tom Amollo <iantom96@gmail.com>
2025-05-27 09:15:41 +0300

    Update models.py

--- server/models.py
+++ server/models.py
@@ -37,8 +37,30 @@ class Post(db.Model):
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
-    # Add validators  
+   @validates('content')
+    def validate_content(self, key, value):
+        if len(value) < 250:
+            raise ValueError("Content must be at least 250 characters long.")
+        return value
 
+    @validates('summary')
+    def validate_summary(self, key, value):
+        if len(value) > 250:
+            raise ValueError("Summary must be at most 250 characters long.")
+        return value
+
+    @validates('category')
+    def validate_category(self, key, value):
+        if value not in ['Fiction', 'Non-Fiction']:
+            raise ValueError("Category must be either Fiction or Non-Fiction.")
+        return value
+
+    @validates('title')
+    def validate_title(self, key, value):
+        clickbait_keywords = ["Won't Believe", "Secret", "Top", "Guess"]
+        if not any(word in value for word in clickbait_keywords):
+            raise ValueError("Title must be clickbait-y.")
+        return value
 
     def __repr__(self):
         return f'Post(id={self.id}, title={self.title} content={self.content}, summary={self.summary})'
